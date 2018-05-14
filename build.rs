8a960398dd8f5692de2fd9e1503155e5bc9c145d
Carl Pearson <carl.w.pearson@gmail.com>
2018-05-14 07:21:00 -0500

    use nvcc to find includes and libraries

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 extern crate find;
 extern crate glob;
 extern crate bindgen;
+extern crate nvcc;
 
 use std::path::PathBuf;
 use std::path::Path;
@@ -8,8 +9,9 @@ use std::vec::Vec;
 use glob::MatchOptions;
 use std::env;
 use find::Find;
+use nvcc::Nvcc;
 
-const SEARCH_LINUX: &[&str] = &["/usr/local/cuda/lib*", "/usr/local/cuda*/lib*"];
+const NVCC_SEARCH_LINUX: &[&str] = &["/usr/local/cuda/bin", "/usr/local/cuda*/bin"];
 
 pub fn main() {
     println!("cargo:rerun-if-changed=build.rs");
@@ -21,12 +23,32 @@ pub fn main() {
 
 
     // Try to use NVCC to get the cuda include and library paths
+    let nvcc = match Find::new("nvcc")
+        .search_env("NVCC_PATH")
+        .search_globs(NVCC_SEARCH_LINUX)
+        .execute()
+    {
+        Ok(path) => Nvcc::new(path).unwrap(),
+        Err(message) => panic!(message),
+    };
+
+
+    let includes = nvcc.includes().iter()
+    .map(|s| format!("-I{}", s.to_str().unwrap()))
+    .collect::<Vec<_>>();
+    eprintln!("nvcc includes: {:?}", includes);
+
+    let libraries = nvcc.libraries().iter()
+    .map(|s| s.to_str().unwrap())
+    .collect::<Vec<_>>();
+    eprintln!("nvcc libraries: {:?}", libraries);
+
 
     // Build runtime bindings
     bindgen::Builder::default()
     // .trust_clang_mangling(false)
     .header("runtime.h")
-    .clang_arg(format!("-I{}", "/usr/local/cuda/include"))
+    .clang_args(&includes)
     .opaque_type("max_align_t")
     .generate()
     .expect("Unable to generate bindings")
@@ -37,7 +59,7 @@ pub fn main() {
     bindgen::Builder::default()
     // .trust_clang_mangling(false)
     .header("driver.h")
-    .clang_arg(format!("-I{}", "/usr/local/cuda/include"))
+    .clang_args(&includes)
     .opaque_type("max_align_t")
     .generate()
     .expect("Unable to generate bindings")
@@ -47,8 +69,8 @@ pub fn main() {
     // Try to find the CUDA libraries
 
     let cudart_path = match Find::new("libcudart.so.*")
-        .search_env("LIBCUDA_PATH")
-        .search_globs(SEARCH_LINUX)
+        .search_env("LIBCUDART_PATH")
+        .search_globs(&libraries)
         .execute()
     {
         Ok(path) => path,
